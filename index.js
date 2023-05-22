4b7174a77ea88ec6b963dda0ff900e2eedea85fc
Juan Salazar <ferthelet@gmail.com>
2023-05-21 22:20:59 -0500

    adds the configObj and the fetch

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+// js
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        dogName: "Byron",
+        dogBreed: "Poodle"
+    }),
+};
+
+fetch("http://localhost:3000/dogs", configurationObject);
+
+
+
+
